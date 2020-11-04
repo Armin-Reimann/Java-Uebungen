350413d15952ad298ab1856aa38ec17edfe9c622
Armin-Reimann <reimann.armin2@gmail.com>
2020-11-04 22:06:33 +0100

    aufgeräumt

--- src/tic_tac_toe/Main.java
+++ src/tic_tac_toe/Main.java
@@ -8,10 +8,8 @@ public class Main {
         // Hier wird der Spieler gespeichert, der gerade am Zug ist
         int turn = 0;
         int winner = -1;
-//        char[] board = new char[9]; //Array mit 9 Feldern
         Board board = new Board();
         Scanner scanner = new Scanner(System.in);
-//        Arrays.fill(board.getInhalt(), '-');
         Player a = new Player(0);
         Player b = new Player(1);
         System.out.println("Namen für ersten Spieler eingeben!");
@@ -35,61 +33,22 @@ public class Main {
             int turnField = players[turn].getTurn(board.getInhalt());
             if(players[turn].validateTurn(board.getInhalt(), turnField)){
                 if(players[turn].number == 0){
-//                    board.getInhalt()[turnField] = 'O';
                     board.setInhalt(turnField, 'O');
                 }else{
-//                    board.getInhalt()[turnField] = 'X';
                     board.setInhalt(turnField, 'X');
                 }
                 //modulo! 0 1 0 1 0 1 ... da nur zwei Spieler
                 turn = ( turn + 1) % 2;
             }
             winner = board.checkWinner();
-//            winner = checkWinner(board.getInhalt());
         }
         if(winner == 0){
             System.out.println(a.name + " hat gewonnen!");
         }else{
             System.out.println(b.name + " hat gewonnen!");
         }
-
     }
-//    public static int checkWinner(char[] board){
-//        //Horizontal (Zeilen)
-//        int winner = -1;
-//        if(board[0] == board[1] && board[0] == board[2] && board[0] != '-'){
-//            winner = board[0];
-//        }
-//        else if(board[3] == board[4] && board[3] == board[5] && board[3] != '-'){
-//            winner = board[3];
-//        }
-//        else if(board[6] == board[7] && board[6] == board[8] && board[6] != '-'){
-//            winner = board[6];
-//            //Senkrecht (Spalten)
-//        }
-//        else if(board[0] == board[3] && board[0] == board[6] && board[0] != '-'){
-//            winner = board[0];
-//        }
-//        else if(board[1] == board[4] && board[1] == board[7] && board[1] != '-'){
-//            winner = board[1];
-//        }
-//        else if(board[2] == board[5] && board[2] == board[8] && board[2] != '-'){
-//            winner = board[2];
-//            //Diagonal
-//        }
-//        else if(board[0] == board[4] && board[0] == board[8] && board[0] != '-'){
-//            winner = board[0];
-//        }
-//        else if(board[2] == board[4] && board[2] == board[6] && board[2] != '-'){
-//            winner = board[2];
-//        }
-//        if(winner == 88){ // wegen ASCII
-//            winner = 1;
-//        }else if(winner == 79){
-//            winner = 0;
-//        }
-//        return winner;
-//    }
+
     public static void printBoard(char[] meinBoard){
         System.out.println(meinBoard[0] + " | " + meinBoard[1] + " | " + meinBoard[2]);
         System.out.println("____________");

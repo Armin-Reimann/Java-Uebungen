8dc755e520aaaffb05c829673a2388e9f04ad7c9
Armin-Reimann <reimann.armin2@gmail.com>
2020-11-10 19:59:22 +0100

    zweites swing fenster

--- src/swing/FirstSwingExample.java
+++ src/swing/FirstSwingExample.java
@@ -1,16 +1,20 @@
 package swing;
 import javax.swing.*;
 
-public class FirstSwingExample {
-    public static void main(String args[]){
-        JFrame f = new JFrame();
-        JButton b = new JButton();
+import javax.swing.*;
+public class FirstSwingExample extends JFrame {
+    JFrame f;
+    public FirstSwingExample(){
+        JButton b=new JButton("click");//create button
+        b.setBounds(130,100,100, 40);
 
-        b.setBounds( 130 , 100 , 100 ,  40);
+        add(b);//adding button on frame
+        setSize(400,500);
+        setLayout(null);
+        setVisible(true);
+    }
 
-        f.add(b);
-        f.setSize(400,500);
-        f.setLayout(null);
-        f.setVisible(true);
+    public static void main(String[] args) {
+        new FirstSwingExample();
     }
 }

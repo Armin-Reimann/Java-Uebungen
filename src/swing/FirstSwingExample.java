864b9a09f94816d9073f6a5785809dd188363532
Armin-Reimann <reimann.armin2@gmail.com>
2020-11-11 20:11:37 +0100

    java swing button mit ActionListener, der ein TextField füllt

--- src/swing/FirstSwingExample.java
+++ src/swing/FirstSwingExample.java
@@ -1,20 +1,22 @@
 package swing;
+
+import java.awt.event.*;
 import javax.swing.*;
-
-import javax.swing.*;
-public class FirstSwingExample extends JFrame {
-    JFrame f;
-    public FirstSwingExample(){
-        JButton b=new JButton("click");//create button
-        b.setBounds(130,100,100, 40);
-
-        add(b);//adding button on frame
-        setSize(400,500);
-        setLayout(null);
-        setVisible(true);
-    }
-
+public class FirstSwingExample {
     public static void main(String[] args) {
-        new FirstSwingExample();
+        JFrame f=new JFrame("Button Example");
+        final JTextField tf=new JTextField();
+        tf.setBounds(50,50, 150,20);
+        JButton b=new JButton("Click Here");
+        b.setBounds(50,100,95,30);
+        b.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                tf.setText("Welcome to Javatpoint.");
+            }
+        });
+        f.add(b);f.add(tf);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
     }
-}
+}  

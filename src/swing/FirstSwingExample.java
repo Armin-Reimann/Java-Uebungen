bbc66c413045b68c7fb3c6d9decd4ed4a0a4cdc3
Armin-Reimann <reimann.armin2@gmail.com>
2020-11-11 22:01:55 +0100

    Klassenstruktur für die GUI-Darstellung aufbauen

--- src/swing/FirstSwingExample.java
+++ src/swing/FirstSwingExample.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 import javax.swing.*;
 public class FirstSwingExample {
     public static void main(String[] args) {
-        JFrame f = new JFrame("Button Example");
+        TTTFrame f = new TTTFrame("Button Example");
         f.setTitle("Tic Tac Toe by Armin Reimann");
         //erstellt die beiden X und O Icons
         //todo noch bilder für einen sieg einbinden
@@ -12,37 +12,21 @@ public class FirstSwingExample {
         i_o = new ImageIcon("src/swing/O.png");
         i_x = new ImageIcon("src/swing/X.png");
 
-
-
-        final JTextField tf=new JTextField();
-        tf.setBounds(50,50, 150,20);
+        // ab hier beginnt das Tic Tac Toe Spiel
         JButton[] b = new JButton[9];
-
         for(int i=0; i<=8; i++){
             b[i] = new JButton(new ImageIcon("src/swing/X.png"));
-            int[] buttonBound = getButtonBounds(i);
+            int[] buttonBound = f.getButtonBounds(i, 10, 10,150,150,10 );
             b[i].setBounds(buttonBound[0],buttonBound[1],buttonBound[2],buttonBound[3]);
         }
         for(int i=0; i<=8; i++){
             f.add(b[i]);
         }
-//        f.add(b);f.add(tf);
+//        f.add(b);
         f.setSize(800,800);
         f.setLayout(null);
         f.setVisible(true);
     }
 
-    public static int[] getButtonBounds(int i){
-        int[][] buttonBounds = new int[9][4];
-        buttonBounds[0] = new int[]{10, 10, 150, 150};
-        buttonBounds[1] = new int[]{170, 10, 150, 150};
-        buttonBounds[2] = new int[]{330, 10, 150, 150};
-        buttonBounds[3] = new int[]{10, 170, 150, 150};
-        buttonBounds[4] = new int[]{170, 170, 150, 150};
-        buttonBounds[5] = new int[]{330, 170, 150, 150};
-        buttonBounds[6] = new int[]{10, 330, 150, 150};
-        buttonBounds[7] = new int[]{170, 330, 150, 150};
-        buttonBounds[8] = new int[]{330, 330, 150, 150};
-        return buttonBounds[i];
-    }
+
 }  
---
+++ src/swing/TTTFrame.java
@@ -0,0 +1,31 @@
+package swing;
+
+import javax.swing.*;
+
+public class TTTFrame extends JFrame {
+    Icon i_o, i_x;
+    public TTTFrame(String button_example) {
+        super();
+        this.i_o = new ImageIcon("src/swing/O.png");
+        this.i_x = new ImageIcon("src/swing/X.png");
+    }
+
+    public int[] getButtonBounds(int key, int x_start, int y_start, int button_breite , int button_hoehe , int abstand){
+        int[][] buttonBounds = new int[9][4];
+//        int x_start = 10;
+//        int y_start = 10;
+//        int button_breite = 150;
+//        int button_hoehe = 150;
+//        int abstand = 10;
+        buttonBounds[0] = new int[]{x_start, y_start, button_breite, button_hoehe};
+        buttonBounds[1] = new int[]{x_start+(abstand+button_breite), y_start, button_breite, button_hoehe};
+        buttonBounds[2] = new int[]{x_start+((abstand+button_breite)*2), y_start, button_breite, button_hoehe};
+        buttonBounds[3] = new int[]{x_start, y_start+(abstand+button_hoehe), button_breite, button_hoehe};
+        buttonBounds[4] = new int[]{x_start+(abstand+button_breite), y_start+(abstand+button_hoehe), button_breite, button_hoehe};
+        buttonBounds[5] = new int[]{x_start+((abstand+button_breite)*2), y_start+(abstand+button_hoehe), button_breite, button_hoehe};
+        buttonBounds[6] = new int[]{x_start, y_start+((abstand+button_hoehe)*2), button_breite, button_hoehe};
+        buttonBounds[7] = new int[]{x_start+(abstand+button_breite), y_start+((abstand+button_hoehe)*2), button_breite, button_hoehe};
+        buttonBounds[8] = new int[]{x_start+((abstand+button_breite)*2), y_start+((abstand+button_hoehe)*2), button_breite, button_hoehe};
+        return buttonBounds[key];
+    }
+}

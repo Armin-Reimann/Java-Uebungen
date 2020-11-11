b1d7f61fbd102941566ec0445f6dacfd07fbd956
Armin-Reimann <reimann.armin2@gmail.com>
2020-11-11 20:34:15 +0100

    O und X Bilder in passender größe (150x150) hinzugefügt

--- src/swing/FirstSwingExample.java
+++ src/swing/FirstSwingExample.java
@@ -7,16 +7,15 @@ public class FirstSwingExample {
         JFrame f=new JFrame("Button Example");
         final JTextField tf=new JTextField();
         tf.setBounds(50,50, 150,20);
-        JButton b=new JButton(new ImageIcon("O.png"));
-
-        b.setBounds(100,100,100,100);
+        JButton b=new JButton(new ImageIcon("src/swing/O.png"));
+        b.setBounds(10,10,270,270);
         b.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 tf.setText("Welcome to Javatpoint.");
             }
         });
         f.add(b);f.add(tf);
-        f.setSize(400,400);
+        f.setSize(800,800);
         f.setLayout(null);
         f.setVisible(true);
     }

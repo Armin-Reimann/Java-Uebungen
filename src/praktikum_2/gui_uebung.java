4539e6046a0f49d252b498c251c9e4de312125ae
Armin-Reimann <reimann.armin2@gmail.com>
2020-11-12 14:02:10 +0100

    verbessert

--- src/praktikum_2/gui_uebung.java
+++ src/praktikum_2/gui_uebung.java
@@ -2,9 +2,6 @@ package praktikum_2;
 
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.stream.IntStream;
 
 public class gui_uebung
 {
@@ -17,7 +14,7 @@ public class gui_uebung
         DefaultListModel<String> l1 = new DefaultListModel<>();
 
         JList<String> list = new JList<>(l1);
-        list.setBounds(0,150, 200,100);
+        list.setBounds(40,150, 200,100);
 
         t1.setBounds(40,100, 200,30);
 
@@ -26,14 +23,17 @@ public class gui_uebung
         b.addActionListener(e -> {
             long[] auswertung = auswerten(t1.getText());
             l1.addElement("Anzahl Vokale: "+String.valueOf(auswertung[0]));
+            l1.addElement("Anzahl Konsonanten: "+String.valueOf(auswertung[3]));
             l1.addElement("Anzahl Zeichen: "+String.valueOf(auswertung[1]));
             l1.addElement("Anzahl Satzzeichen: "+String.valueOf(auswertung[2]));
+
         });
 
         f.add(list);
         f.add(b);
         f.add(t1);
         f.setSize(400,400);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setLayout(null);
         f.setVisible(true);
     }
@@ -43,6 +43,7 @@ public class gui_uebung
         auswertung[0] = text.chars().filter(ch -> ch == 'e' || ch == 'a' || ch == 'o' || ch == 'i' || ch == 'u').count();
         auswertung[1] = text.chars().filter(ch -> ch != ' ').count();
         auswertung[2] = text.chars().filter(ch -> ch == '.' || ch == ';' || ch == ',' || ch == ':' || ch == '-' || ch == '_').count();
+        auswertung[2] = text.chars().filter(ch -> ch != 'e' && ch != 'a' && ch != 'o' && ch != 'i' && ch != 'u').count();
         return auswertung;
     }
 

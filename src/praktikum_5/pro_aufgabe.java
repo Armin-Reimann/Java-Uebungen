bd857ff831763ff4c9e8ea1b53f3d4042ccd156c
Armin-Reimann <reimann.armin2@gmail.com>
2020-12-03 15:46:07 +0100

    pro aufgabe praktikum 5 vereinfacht

--- src/praktikum_5/pro_aufgabe.java
+++ src/praktikum_5/pro_aufgabe.java
@@ -19,39 +19,14 @@ public class pro_aufgabe {
                 7432873047312L,
                 7832480324983L,
                 7832480324983L};
-        long result = findSingle1xFor(eingabe);
-        System.out.println(result);
-
-    }
-    //Methode mit 2x For-Schleife
-    private static long findSingle2xFor(long[] arr) {
-        boolean check = true;
-
-        for(int i = 0; i < arr.length ; i++ ) {
-            //System.out.println(eingabe[i]);
-            for(int j = 0 ; j < arr.length ; j++ ) {
-                if(arr[i] == arr[j] && i != j)
-                    check = false;
-            }
-            if(check)
-                System.out.println(arr[i]);
-            check = true;
-        }
-        return 0;
+        System.out.println(findSingle1xFor(eingabe));
     }
 
-
-    //Methode mit 1x For-Schleife
     private static long findSingle1xFor(long[] arr) {
-
-        // "^" = XOR
         long result = arr[0];
-        //System.out.println("=: " + result);
         for(int i = 1 ; i < arr.length ; i++ ) {
             result = result ^ arr[i];
-            //System.out.println(i + ": " + result + "   ----" + arr[i]);
         }
-        System.out.println(result);
         return result;
     }
 }

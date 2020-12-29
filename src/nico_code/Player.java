5a1938129f70ba3c7d5eb355d33e09ea3850a088
ItsNico01 <n1c0undso0901@gmail.com>
2020-12-29 17:04:00 +0100

    Name

--- src/nico_code/Player.java
+++ src/nico_code/Player.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Player {
     protected int number;
     protected String zeichen;
+    protected String name;
     protected Scanner scanner = new Scanner(System.in);
 
     public Player(int number){
@@ -14,6 +15,17 @@ public class Player {
         }else if (number == 1){
             zeichen = "X";
         }
+
+    }
+
+
+    public void setName() {
+        System.out.println("Spieler " + (number+1) + ", geb deinen Namen ein:");
+        name = scanner.next();
+    }
+
+    public String getName () {
+        return name;
     }
 
     public String getZeichen() {
@@ -21,7 +33,7 @@ public class Player {
     }
 
     public int[] getTurn(){
-       System.out.println("Player" + number + " - du bist am Zug! ");
+       System.out.println(name + " - du bist am Zug! ");
        int input = scanner.nextInt();
        if (input < 9 && input >= 0){
            return getMapping(input);
